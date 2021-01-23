2ad7a553357afdbed04b7189715c90043b377eef
pv <parshav@live.com>
2021-01-23 12:45:55 -0600

    Update style and custom view to align latest changes from https://github.com/ServerDriven/ScreenData-swift/pull/7/files

--- ScreenDataModels/src/main/java/com/pv/screendata/objects/SomeStyle.kt
+++ ScreenDataModels/src/main/java/com/pv/screendata/objects/SomeStyle.kt
@@ -5,9 +5,11 @@ import com.pv.screendata.types.Alignment
 data class SomeStyle(
     val backgroundColor: SomeColor? = null,
     val foregroundColor: SomeColor? = null,
-    val isHidden: Boolean,
+    val isHidden: Boolean = false,
     val cornerRadius: Int,
     val paddingStart: Int = 0,
-    val paddingEnd: Int = 0, // default 0 instead null for padding ?,
+    val paddingEnd: Int = 0,
+    val height: Int? = null,
+    val width: Int? = null,
     val alignment: Alignment
 )
--- ScreenDataModels/src/main/java/com/pv/screendata/views/SomeCustomView.kt
+++ ScreenDataModels/src/main/java/com/pv/screendata/views/SomeCustomView.kt
@@ -7,14 +7,14 @@ import com.pv.screendata.types.ViewDirectionAxis
 import com.pv.screendata.types.ViewType
 
 data class SomeCustomView(
-    val id: String? = null,
-    val title: String,
+    val id: String, // not sure if it needs to be null entirely.
+    val title: String? = null,
     val subtitle: String? = null,
     val someStyle: SomeStyle? = null,
     val someImage: SomeImage? = null,
     val destination: Destination? = null,
-    val axis: ViewDirectionAxis,
-    val someViews: Array<SomeView>
+    val axis: ViewDirectionAxis? = null,
+    val someViews: List<SomeView>? = null
 ) {
     val type: ViewType = ViewType.custom
 }
